51588b5c59eb900dd3f7b03d3cd07849d925f422
alsrx <alineramos022002@gmail.com>
2020-03-02 10:06:58 -0300

    noticias

---
+++ aula.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <h2>Portal Aline de Notícias</h2>
+
+  <div class="noticias">
+    <h3>BBB20</h3>
+
+    <img src="https://upload.wikimedia.org/wikipedia/pt/b/b2/Bloggif_5e236b0773b79.png">
+
+    <p> Em paredão triplo BBB20 surpreende público eliminando dois participantes de uma só vez.</p>
+    <p> A eliminação aconteceu antes da hora, pois ninguém aguentava mais!!</p>
+    <p> Pyong e Guilherme receberam a notícia e saíram da casa chocados</p>
+
+  </div>
+
+  <div class="noticias">
+    <h3>EXPO PARANAVAÍ 2020</h3>
+
+    <p>A Exposição Agropecuária de Paranavaí (ExpoParanavaí) 2020, no noroeste do Paraná, será realizada de 6 a 15 de março.</p>
+    <p>Confira valor para show!</p>
+
+    <table>
+      <tr>
+        <th>SHOW</th>
+        <th>VALOR-inteira</th>
+      </tr>
+      <tr>
+        <td>Marilia Mendonça</td>
+        <td>48,00</td>
+      </tr>
+    </table>
+
+
+  </div>
+
+  <div class="noticia">
+    <h3>Campus Paranavaí realiza formatura dos cursos técnicos integrados</h3>
+
+    <img src="https://paranavai.ifpr.edu.br/wp-content/uploads/2020/02/87284587_2767870066642616_5368864271398600704_n.jpg" >
+
+    <p>Na última sexta-feira, dia 14/02, o IFPR – Campus Paranavaí realizou a colação de grau de 72 técnicos de</p>
+    <p>Agroindústria, Eletromecânica e Informática!</p>
+
+  </div>
+
+</body>
+</html>
