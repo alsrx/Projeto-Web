2aebc190affd5f0a49d530c9b51137bd79373d62
Aluno <alineramos022002@gmail.com>
2020-03-09 09:34:02 -0300

    meu_site

---
+++ aula04.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Aula 04 - CSS e HTML</title>
+  <!--Toda imortação de CSS deve ser feita no HEAD -->
+  <link rel="stylesheet" href="aula04.css">
+</head>
+<body>
+
+  <div class="site">
+
+    <div class="topo">
+      Topo do Site
+    </div>
+
+    <div class="conteudo">
+      Corpo do Site
+    </div>
+
+    <div class="rodape">
+      Desenvolvido por Aline Ramos
+    </div>
+
+  </div>
+
+</body>
+</html>
