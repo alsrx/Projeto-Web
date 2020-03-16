60480438890ade42f24dddc32f218f5b4d79d311
alsrx <alineramos022002@gmail.com>
2020-03-16 10:05:00 -0300

    meu_site

---
+++ aula05.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Aula 05 - CSS e HTML</title>
+  <!--Toda imortação de CSS deve ser feita no HEAD -->
+  <link rel="stylesheet" href="aula05.css">
+</head>
+<body>
+
+  <div class="site">
+
+    <div class="topo">
+
+      <h2>Criação de menu e botões</h2>
+
+      <div class="menu">
+        <ul>
+          <li><a href="#">Página Inicial</a></li>
+          <li><a href="#">Cadastro</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Ajuda</a></li>
+          <li><a href="#">Login</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div class="conteudo">
+
+      <h3>Criação de botões</h3>
+
+      <a href="#" class= "botao botao-azul">Botão Azul</a>
+      <a href="#" class= "botao botao-verde">Botão Verde</a>
+      <a href="#" class= "botao botao-vermelho">Botão Vermelho</a>
+
+
+      
+    </div>
+
+    <div class="rodape">
+      Desenvolvido por Aline Ramos
+    </div>
+
+  </div>
+
+</body>
+</html>
